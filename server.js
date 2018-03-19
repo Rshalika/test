daf39aa9beab56cb66c0c2c0d1b32eebd25ac87b
Rshalika <r.shalika08@gmail.com>
2018-03-19 04:47:13 +0400

    test

--- server.js
+++ server.js
@@ -12,13 +12,12 @@ var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 
-
-
 app.get('/', function (req, res) {
-
     res.render('index.html', { pageCountMessage : null});
 
 });
+
+
 
 
 
