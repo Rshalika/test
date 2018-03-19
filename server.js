cc7ed2e3dcdc966aa8ccc9091fe48735e5163a1a
Rshalika <r.shalika08@gmail.com>
2018-03-19 08:58:11 +0400

    add ebay pages

--- server.js
+++ server.js
@@ -38,6 +38,109 @@ app.get('/delcampeItem', function (req, res) {
 
 });
 
+app.get('/buy/browse/v1/item/*', function (req, res) {
+
+    res.status(200).send({
+        "itemId": "v1|263557932538|0",
+        "title": "Breyer 710112 - W.H. Annie - 2002 BreyerFest Special Run - 1000 Made",
+        "shortDescription": "Perfect condition 2002 Breyerfest W.H. Annie A little dust on her but no scratches.",
+        "price": {
+            "value": "64.99",
+            "currency": "USD"
+        },
+        "categoryPath": "Collectibles|Animals|Horses: Model Horses|Breyer|Traditional, 12 x 9 Inch",
+        "condition": "New",
+        "conditionId": "1000",
+        "itemLocation": {
+            "city": "Farmington",
+            "stateOrProvince": "New Mexico",
+            "postalCode": "87401",
+            "country": "US"
+        },
+        "image": {
+            "imageUrl": "https://i.ebayimg.com/00/s/MTE0MlgxNjAw/z/fW0AAOSwuoxamgaW/$_57.JPG?set_id=8800005007"
+        },
+        "additionalImages": [
+            {
+                "imageUrl": "https://i.ebayimg.com/00/s/MTYwMFgxMTQy/z/9xQAAOSwGvZamgaa/$_57.JPG?set_id=8800005007"
+            },
+            {
+                "imageUrl": "https://i.ebayimg.com/00/s/MTE0MlgxNjAw/z/ZxkAAOSw9hlamgae/$_57.JPG?set_id=8800005007"
+            },
+            {
+                "imageUrl": "https://i.ebayimg.com/00/s/MTE0MlgxNjAw/z/o2UAAOSw3Hlamgak/$_57.JPG?set_id=8800005007"
+            }
+        ],
+        "currentBidPrice": {
+            "value": "44.99",
+            "currency": "USD"
+        },
+        "itemEndDate": "2018-03-26T04:37:46.000Z",
+        "seller": {
+            "username": "ristyz",
+            "feedbackPercentage": "100.0",
+            "feedbackScore": 162
+        },
+        "estimatedAvailabilities": [
+            {
+                "deliveryOptions": [
+                    "SHIP_TO_HOME"
+                ],
+                "estimatedAvailabilityStatus": "IN_STOCK",
+                "estimatedAvailableQuantity": 1,
+                "estimatedSoldQuantity": 0
+            }
+        ],
+        "shippingOptions": [
+            {
+                "shippingServiceCode": "Expedited Shipping",
+                "type": "Expedited shipping",
+                "shippingCost": {
+                    "value": "15.00",
+                    "currency": "USD"
+                },
+                "quantityUsedForEstimate": 1,
+                "minEstimatedDeliveryDate": "2018-03-29T10:00:00.000Z",
+                "maxEstimatedDeliveryDate": "2018-04-03T10:00:00.000Z",
+                "additionalShippingCostPerUnit": {
+                    "value": "0.00",
+                    "currency": "USD"
+                },
+                "shippingCostType": "FIXED"
+            }
+        ],
+        "shipToLocations": {
+            "regionIncluded": [
+                {
+                    "regionName": "United States",
+                    "regionType": "COUNTRY"
+                }
+            ]
+        },
+        "returnTerms": {
+            "returnsAccepted": false
+        },
+        "topRatedBuyingExperience": false,
+        "buyingOptions": [
+            "FIXED_PRICE",
+            "AUCTION"
+        ],
+        "itemAffiliateWebUrl": "https://rover.ebay.com/rover/1/711-53200-19255-0/1?campid=%3CePNCampaignId%3E&customid=%3CreferenceId%3E&toolid=10050&mpre=https%3A%2F%2Fwww.ebay.com%2Fitm%2FBreyer-710112-W-H-Annie-2002-BreyerFest-Special-Run-1000-Made-%2F263557932538",
+        "itemWebUrl": "https://www.ebay.com/itm/Breyer-710112-W-H-Annie-2002-BreyerFest-Special-Run-1000-Made-/263557932538",
+        "description": "<font rwr='1' size='4' style='font-family:Arial'>Perfect condition 2002 Breyerfest W.H. Annie&nbsp; A little dust on her but no scratches. <br></font>",
+        "uniqueBidderCount": 0,
+        "adultOnly": false,
+        "categoryId": "35999"
+    })
+
+
+});
+
+app.get('/buy/browse/v1/item/get_items_by_item_group', function (req, res) {
+    res.status(404).send()
+
+});
+
 
 app.use(function (err, req, res, next) {
     console.error(err.stack);
